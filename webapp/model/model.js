4a0a2aa5ae2eee91becc5218fd414b683bf29048
MissRen <misshikariren@gmail.com>
2019-05-22 15:07:43 +0000

    work code

--- webapp/model/model.js
+++ webapp/model/model.js
@@ -4,9 +4,24 @@ sap.ui.define([
 	"use strict";
 
 	XMLView.create({
-		viewName: "final-project-portal.webapp.view.App"
+		viewName: "final-project-portal.webapp.view.Login"
 	}).then(function (oView) {
 		oView.placeAt("content");
 	});
 
 });
+
+// sap.ui.define([
+// 	"sap/ui/model/json/JSONModel",
+// 	"sap/ui/Device"
+// ], function (JSONModel, Device) {
+// 	"use strict";
+
+// 	return {
+// 		createDeviceModel : function () {
+// 			var oModel = new JSONModel(Device);
+// 			oModel.setDefaultBindingMode("OneWay");
+// 			return oModel;
+// 		}
+// 	};
+// });
