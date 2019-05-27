e344542faa72c8280b30118ed5a9c66fa44c27cf
MissRen <misshikariren@gmail.com>
2019-05-27 17:59:34 +0200

    add code in conzroller index

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -3,21 +3,18 @@ sap.ui.define(["sap/ui/core/mvc/Controller"],
 	"use strict";
 	return Controller.extend("myhelper.webapp.controller.Login", {
 		checkUser: function (oEvent) {
-			var user = this.byId('emailLogin').getValue();
-			var password = this.byId('password').getValue();
+			var user = this.byId("emailLogin").getValue();
+			this.byId("password").getValue();
 			sap.m.MessageToast.show("Benutzername/E-Mail " + user + " " + this._validateUser(user));
-			_validateUser(user);
-			_openHome();
 		},
 		_validateUser: function (sUserName) {
 			var aUsers = ["Max", "Jana", "Thomas"]; // should retrieved from somewhere, e.g., JSON file or Model
 			return aUsers.indexOf(sUserName) > -1;
-		}
-
+		},
 		_openHome: function() {
-		var view = sap.ui.view({id:"",viewName:"myhelper.webapp.view.Home",
+		sap.ui.view({id:"",viewName:"myhelper.webapp.view.Home",
 		typ:sap.ui.core.mvc.ViewTyp.Js
-		oPanel.setContent();
+		});
 		}
 	});
 });
