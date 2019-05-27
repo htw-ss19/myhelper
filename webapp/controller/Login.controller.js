5388c3c06b879f6fe50985276811f25aa1a7574a
MissRen <misshikariren@gmail.com>
2019-05-27 17:34:28 +0200

    add code in login controller

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,14 +1,23 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller"],
+ function (Controller) {
 	"use strict";
-	return Controller.extend("myhelper.controller.Login", {
-		onInit: function () {},
+	return Controller.extend("myhelper.webapp.controller.Login", {
 		checkUser: function (oEvent) {
 			var user = this.byId('emailLogin').getValue();
+			var password = this.byId('password').getValue();
 			sap.m.MessageToast.show("Benutzername/E-Mail " + user + " " + this._validateUser(user));
+			_validateUser(user);
+			_openHome();
 		},
 		_validateUser: function (sUserName) {
 			var aUsers = ["Max", "Jana", "Thomas"]; // should retrieved from somewhere, e.g., JSON file or Model
 			return aUsers.indexOf(sUserName) > -1;
+		}
+
+		_openHome: function() {
+		var view = sap.ui.view({id:"",viewName:"myhelper.webapp.view.Home",
+		typ:sap.ui.core.mvc.ViewTyp.Js
+		oPanel.setContent();
 		}
 	});
 });
